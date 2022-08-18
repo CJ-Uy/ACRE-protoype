69827a2cb19f86bdf08bc3d149a4812fb2f5aab9
Charles Joshua Uy <uy.charlesjoshua@yahoo.com>
2022-08-18 19:57:22 +0800

    some very little work on the landing page

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,36 @@
 </script>
 
 <template>
-  <h1>
-    Hello World
-  </h1>
+  <header>
+    <img src="src/assets/images/acre_logo.png" alt="ACRE logo" id="logo">
+    <h1 id="title">ACRE</h1>
+    <nav>
+      <button id="about">ABOUT</button>
+      <button id="plant_base">PLANT BASE</button>
+      <button id="customer_service">CUSTOMER SERVICE</button>
+      <button id="main">MAIN</button>
+    </nav>
+  </header>
+  <main>
+    <h1 id="name">
+      Assisted<br>
+      Cavendish<br>
+      Realtime<br>
+      Emulator
+    </h1>
+    <article>
+      Digitial Twins: Crop Modelling for real-time
+      insights that enables predictive farming
+      and the construction of a prototype with working
+      GUI and Simple Algorithm
+    </article>
+  </main>
+  <div id="banana">
+    <div class="circle" id="c1"></div>
+    <div class="circle" id="c2"></div>
+    <div class="circle" id="c3"></div>
+  </div>
 </template>
-
-<script>
-
-//to source u need to call from main.js
-
-</script>
 
 <style scoped>
   @import "assets/css/main.css";
