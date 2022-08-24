cab5cb8a3d398d1e6050ca1a46a7b5deb8ecf3a1
Charles Joshua Uy <uy.charlesjoshua@yahoo.com>
2022-08-24 22:27:47 +0800

    added calendar

--- src/views/farm_views/FarmCal.vue
+++ src/views/farm_views/FarmCal.vue
@@ -3,24 +3,29 @@
     <h1>CALENDAR</h1>
     <Farm_navbar />
   </div>
-  <div id="calendar">test</div>
+  <div id="calendar">
+    <Farm_calendar />
+  </div>
 </template>
 
 <style scoped>
 @import url("@/assets/css/FarmUniversal.css");
 #calendar {
-  margin-top: 3%;
-  margin-left: 27%;
+  position: absolute;
+  top: 15%;
+  margin-left: 21%;
   width: 70%;
 }
 </style>
 
 <script>
+import Farm_calendar from "@/components/farm_components/farm_calendar.vue";
 import Farm_navbar from "@/components/farm_components/farm_nav.vue";
 
 export default {
   components: {
     Farm_navbar,
+    Farm_calendar,
   },
 };
 </script>
