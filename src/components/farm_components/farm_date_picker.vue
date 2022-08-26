6d6bd1344bc1b54989608b4e72b0a4f7b58cbcaf
Charles Joshua Uy <uy.charlesjoshua@yahoo.com>
2022-08-26 12:19:34 +0800

    added date picker

---
+++ src/components/farm_components/farm_date_picker.vue
@@ -0,0 +1,19 @@
+<template>
+  <Datepicker v-model="date"></Datepicker>
+</template>
+
+<style scoped></style>
+
+<script>
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+export default {
+  components: { Datepicker },
+  data() {
+    return {
+      date: null,
+    };
+  },
+};
+</script>
--- src/views/farm_views/FarmCal.vue
+++ src/views/farm_views/FarmCal.vue
@@ -3,28 +3,36 @@
     <h1>CALENDAR</h1>
     <Farm_navbar />
   </div>
-  <div id="calendar">
-    <Farm_calendar />
+  <div class="farm_main">
+    <div id="farm_date_picker">
+      <Farm_date_picker />
+    </div>
+    <div id="farm_calendar">
+      <Farm_calendar />
+    </div>
   </div>
 </template>
 
 <style scoped>
 @import url("@/assets/css/FarmUniversal.css");
-#calendar {
-  position: absolute;
-  top: 15%;
-  margin-left: 21%;
-  width: 70%;
+#farm_calendar {
+  position: inline-block;
+}
+#farm_date_picker {
+  outline: 2px ssolid green;
+  width: 20%;
 }
 </style>
 
 <script>
 import Farm_calendar from "@/components/farm_components/farm_calendar.vue";
+import Farm_date_picker from "@/components/farm_components/farm_date_picker.vue";
 import Farm_navbar from "@/components/farm_components/farm_nav.vue";
 
 export default {
   components: {
     Farm_navbar,
+    Farm_date_picker,
     Farm_calendar,
   },
 };
